f3ddf4df41cd9a6ac766c79347f21052e000bc9d
online5880 <gjtjqkr1@naver.com>
2022-08-25 22:26:13 +0900

    delete

---
+++ CPP_Part1/modernC++/delete.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+using namespace std;
+#include <vector>
+#include <list>
+#include <deque>
+#include <map>
+#include <set>
+
+// 오늘의 주제 : delete (삭제된 함수)
+
+class Knight
+{
+public:
+
+public:
+	void operator=(const Knight& k) = delete;
+
+	
+
+	// 모든 것을 뚫는 창 vs 절대 방패
+	friend class Admin;
+
+private:
+	int _hp = 100;
+};
+
+class Admin
+{
+public:
+	void CopyKnight(const Knight& k)
+	{
+		Knight k1;
+		// 복사 연산
+		k1 = k;
+	}
+};
+
+int main()
+{
+	Knight k1;
+
+	Knight k2;
+
+	// 복사 연산자
+	k1 = k2;
+
+	Admin admin;
+	admin.CopyKnight(k1);
+	
+	return 0;
+}
